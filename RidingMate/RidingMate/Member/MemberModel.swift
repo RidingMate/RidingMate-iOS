a4171929999f9b4b77facea228d65b37d4841d3c
JunYoung An <shooootingsta@gmail.com>
2022-06-14 14:02:22 +0900

    [FEAT] 회원가입, 로그인 화면구현

---
+++ RidingMate/RidingMate/Member/MemberModel.swift
@@ -0,0 +1,13 @@
+//
+//  MemberModel.swift
+//  RidingMate
+//
+//  Created by 안준영 on 2022/06/14.
+//
+
+import Foundation
+
+enum memberType: String {
+  case signin = "SIGN IN"
+  case login = "LOGIN"
+}
---
+++ RidingMate/RidingMate/Member/MemberViewController.swift
@@ -0,0 +1,88 @@
+//
+//  MemberViewController.swift
+//  RidingMate
+//
+//  Created by 안준영 on 2022/06/14.
+//
+
+import Foundation
+import UIKit
+
+class MemberViewController: UIViewController {
+  
+  // MARK: - Property
+  private var memberTypeCheck: memberType?
+  
+  private let titleMainLabel: UILabel = {
+    let label = UILabel()
+    label.textColor = .black
+    label.sizeToFit()
+    label.clipsToBounds = true
+    return label
+  }()
+  
+  private let titleSubLabel: UILabel = {
+    let label = UILabel()
+    label.textColor = .black
+    label.sizeToFit()
+    label.clipsToBounds = true
+    return label
+  }()
+  
+  // MARK: - init
+  init(memberType: memberType) {
+    self.memberTypeCheck = memberType
+    super.init(nibName: nil, bundle: nil)
+    
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  // MARK: - Lifecycle
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    setNavi()
+    configureUI()
+  }
+  
+  // MARK: - Function
+  private func setNavi() {
+    self.navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
+    self.navigationController?.navigationBar.shadowImage = UIImage()
+    self.navigationController?.navigationBar.isTranslucent = true
+    self.navigationController?.view.backgroundColor = .clear
+    
+    self.navigationController?.hidesBarsOnSwipe = true
+    
+    let imageView = UIImageView(image: UIImage(named: "Back"))
+    imageView.frame = CGRect(origin: .zero, size: CGSize(width: 50, height: 50))
+    imageView.clipsToBounds = true
+    
+    self.navigationItem.leftBarButtonItem = UIBarButtonItem(customView: imageView)
+  }
+}
+
+// MARK: - Layout
+extension MemberViewController {
+  private func configureUI() {
+    [titleMainLabel, titleSubLabel].forEach {
+      view.addSubview($0)
+    }
+    
+    self.titleMainLabel.text = memberTypeCheck?.rawValue
+    self.titleSubLabel.text = memberTypeCheck == .signin ? "회원가입": "로그인"
+    
+    titleMainLabel.snp.makeConstraints {
+      $0.top.equalTo(view.safeAreaLayoutGuide.snp.top).inset(40)
+      $0.centerX.equalToSuperview()
+    }
+    
+    titleSubLabel.snp.makeConstraints {
+      $0.top.equalTo(titleMainLabel.snp.bottom).offset(10)
+      $0.centerX.equalToSuperview()
+    }
+  }
+}
